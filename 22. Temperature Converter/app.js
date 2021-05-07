ba98751347705cd4d35e21ec8bfb8c290f2064e7
the_last_crusaderr <guptasumit922@gmail.com>
2021-05-07 08:39:50 +0530

    First app of js

---
+++ 22. Temperature Converter/app.js
@@ -0,0 +1,9 @@
+// Today's temperature in kelvin
+const kelvin = 0;
+// temperature in celsius
+let celsius = kelvin - 273;
+//temperature in fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
+ 
