03b0521d1355dafb007647d3e7acaeaaa5171f46
the_last_crusaderr <guptasumit922@gmail.com>
2021-05-07 08:50:59 +0530

    Dog years

---
+++ 23. Dog Years/app.js
@@ -0,0 +1,15 @@
+// my current age
+let myAge = 23;
+// Dog's first two years
+let earlyYears = 2;
+earlyYears *= 10.5;
+//Subtraction of first two years from current age
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears,laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'sumit'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+ 
