b4b4609c8d23f247d7f378cce8db7ab3e36c4f7f
the_last_crusaderr <guptasumit922@gmail.com>
2021-05-10 13:35:56 +0530

    rectified whale talk

--- 31. Whale Talk/app.js
+++ 31. Whale Talk/app.js
@@ -2,10 +2,13 @@ let input = "The world is a great place to live, only if you live accordance to 
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = [];
 for(let i = 0;i<input.length;i++){
-  for(let j=0;j<vowels.length;i++){
-        if(input[i] == vowels[j]){
-            if (input[i]=='e' || input[i]=='u'){
+  for(let j=0;j<vowels.length;j++){
+        if(input[i] === vowels[j]){
+            if (input[i] === 'e' || input[i] === 'u'){
               resultArray.push(input[i]);
+              resultArray.push(input[i]);
+            }
+            else{
               resultArray.push(input[i]);
             }
         }
@@ -13,4 +16,4 @@ for(let i = 0;i<input.length;i++){
   }
 }
 
-console.log(resultArray); 
+console.log((resultArray.join('')).toUpperCase());
