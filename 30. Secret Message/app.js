03fdba2b44001fbe6aa2817033c6ed6800675363
the_last_crusaderr <guptasumit922@gmail.com>
2021-05-10 10:23:48 +0530

    secret messages

--- 30. Secret Message/app.js
+++ 30. Secret Message/app.js
@@ -1,1 +1,18 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to','program');
+let index = secretMessage.indexOf('easily');
+secretMessage[index] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+
+let indexOfget = secretMessage.indexOf('get');
+
+secretMessage.splice(indexOfget, 5, 'know')
+
+console.log(secretMessage.join(' '));
  
