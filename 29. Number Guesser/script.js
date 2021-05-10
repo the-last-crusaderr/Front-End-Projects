2eb57a64243d3077a4f5a01ec1b74d01217283a0
the_last_crusaderr <guptasumit922@gmail.com>
2021-05-10 08:51:03 +0530

    number guesser

--- 29. Number Guesser/script.js
+++ 29. Number Guesser/script.js
@@ -4,3 +4,39 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+
+function generateTarget(){
+      let target = Math.floor((Math.random() * 10));
+      return target;
+}
+
+
+function compareGuesses(user, computer, target){
+    let userDiff = Math.abs(target - user);
+    let computerDiff = Math.abs(target - computer);
+    if (userDiff === computerDiff || userDiff < computerDiff)
+        return true;
+    else
+      return false;    
+}
+
+
+function updateScore(winner){
+  if (winner = 'human')
+    humanScore++;
+  else
+    computerScore++;  
+}
+
+
+function advanceRound(){
+  currentRoundNumber++;
+}
+
+
